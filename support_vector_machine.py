32e239f001ab28b08406f1390565271fa0580465
Gabriel Dilly <gabrelsoueu@hotmail.com>
2020-08-20 07:23:49 -0300

    Removing prints and generate_model function

--- support_vector_machine.py
+++ support_vector_machine.py
@@ -34,42 +34,68 @@ def get_dataset(df, base_selected):
 #     return df
 
 #%%
-# Importing the dataset
-dataset = get_dataset(df_16b_dice, 'ElGamal')
+def generate_model(df, base_selected):
+    # Importing the dataset
+    dataset = get_dataset(df, base_selected)    
+    
+    X = dataset.iloc[:, :-2].values
+    y = dataset.iloc[:, -1].values
+    
+    # Splitting the dataset into the Training set and Test set
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+    # print(X_train)
+    # print(y_train)
+    # print(X_test)
+    # print(y_test)
+    
+    # Feature Scaling
+    from sklearn.preprocessing import StandardScaler
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
+    # print(X_train)
+    # print(X_test)
+    
+    # Training the SVM model on the Training set
+    from sklearn.svm import SVC
+    classifier = SVC(kernel = 'linear', random_state = 0)
+    classifier.fit(X_train, y_train)
+    
+    # Predicting the Test set results
+    y_pred = classifier.predict(X_test)
+    print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+    
+    # Making the Confusion Matrix
+    from sklearn.metrics import confusion_matrix, accuracy_score
+    cm = confusion_matrix(y_test, y_pred)
+    print(cm)
+    accuracy_score(y_test, y_pred)
+    
+#%%
+generate_model(df_8b_cos, 'DES')
+generate_model(df_8b_dice, 'DES')
+generate_model(df_8b_ed, 'DES')
 
-X = dataset.iloc[:, :-2].values
-y = dataset.iloc[:, -1].values
+generate_model(df_16b_cos, 'DES')
+generate_model(df_16b_dice, 'DES')
+generate_model(df_16b_ed, 'DES')
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-print(X_train)
-print(y_train)
-print(X_test)
-print(y_test)
+generate_model(df_8b_cos, 'RSA')
+generate_model(df_8b_dice, 'RSA')
+generate_model(df_8b_ed, 'RSA')
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-print(X_train)
-print(X_test)
+generate_model(df_16b_cos, 'RSA')
+generate_model(df_16b_dice, 'RSA')
+generate_model(df_16b_ed, 'RSA')
 
-# Training the SVM model on the Training set
-from sklearn.svm import SVC
-classifier = SVC(kernel = 'linear', random_state = 0)
-classifier.fit(X_train, y_train)
+generate_model(df_8b_cos, 'ElGamal')
+generate_model(df_8b_dice, 'ElGamal')
+generate_model(df_8b_ed, 'ElGamal')
 
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
-
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix, accuracy_score
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
-accuracy_score(y_test, y_pred)
+generate_model(df_16b_cos, 'ElGamal')
+generate_model(df_16b_dice, 'ElGamal')
+generate_model(df_16b_ed, 'ElGamal')
 
 #%%
 # Visualising the Training set results
